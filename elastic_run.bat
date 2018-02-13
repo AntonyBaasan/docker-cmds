b71fcc39c2b167e37216aa756513a62c12cd2118
Ankhbayar <antony.baasan@gmail.com>
2018-02-12 20:23:44 -0500

    fix elastic location

--- elastic_run.bat
+++ elastic_run.bat
@@ -1,4 +1,4 @@
-REM docker run --name my_elastic -d -p 9200:9200 -t solr
+REM docker run --name my_elastic -d -p 9200:9200 -t docker.elastic.co/elasticsearch/elasticsearch:6.2.1
 
 REM below steps are done in order to port forwarding
 REM https://stackoverflow.com/questions/19335444/how-do-i-assign-a-port-mapping-to-an-existing-docker-container
